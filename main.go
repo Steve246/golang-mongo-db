8485ce735d142da4cac16ad3932f0215e5a39643
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-07-12 15:18:23 +0700

    add

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -160,20 +161,20 @@ func main() {
 
 	//update
 
-	filterId, _ := primitive.ObjectIDFromHex("62ccfe1d8412e9e781a34716")
+	// filterId, _ := primitive.ObjectIDFromHex("62ccfe1d8412e9e781a34716")
 
 	// filterData := bson.D{{"_id", "62ccfe1d8412e9e781a34716"}}
 
-	updateData := bson.D{{
-		"$set", bson.D{{
-			Key: "senior", Value: true,
-		}},
-	}}
+	// updateData := bson.D{{
+	// 	"$set", bson.D{{
+	// 		Key: "senior", Value: true,
+	// 	}},
+	// }}
 
 	// result, err := coll.UpdateOne(ctx, filterId, updateData)
-	result, _ := coll.UpdateOne(ctx, bson.M{"_id": filterId}, updateData)
+	// result, _ := coll.UpdateOne(ctx, bson.M{"_id": filterId}, updateData)
 
-	fmt.Printf("Documents updated: %v\n", result.MatchedCount)
+	// fmt.Printf("Documents updated: %v\n", result.MatchedCount)
 
 
 
@@ -190,6 +191,184 @@ func main() {
 	// }
 
 	// fmt.Printf("DeleteOne removed %v document(s)\n", result.DeletedCount)
+
+
+	//Read -- Find
+
+	// cursor, err := coll.Find(ctx, bson.D{})
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	// var students []bson.D //nama collections, jangan sampe salah
+	// err = cursor.All(ctx, &students)
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	// for _, student := range students {
+	// 	fmt.Println(student)
+	// }
+
+	//projection 
+
+	// opts := options.Find().SetProjection(bson.D{
+	// 	{"_id", 0},
+	// 	{"name", 1},
+	// 	{"age", 1},
+	// })
+
+	// cursor, err := coll.Find(ctx, bson.D{}, opts)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// var students []bson.D
+
+	// err = cursor.All(ctx, &students)
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	// for _, student := range students {
+	// 	fmt.Println(student)
+	// }
+
+	//Logical
+
+	// var students []bson.D
+
+	// filterGenderAndAge := bson.D{
+	// 	{"$and", bson.A{
+	// 		bson.D{
+	// 			{"gender", "F"},
+	// 			{"age", bson.D{{"$gte", 25}}},
+	// 		},
+	// 	}},
+	// }
+
+	// projection := bson.D{
+	// 	{"_id", 1}, 
+	// 	{"fullName", 1},
+	// 	{"gender", 1},
+	// 	{"age", 1},
+	// }
+
+	// cursor, err := coll.Find(ctx, filterGenderAndAge, options.Find().SetProjection(projection))
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	// err = cursor.All(ctx, &students)
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	// for _, student := range students {
+	// 	fmt.Println("FILTER BY GENDER & AGE", student)
+	// }
+
+	//merging result querry ke struct
+
+	// var students []bson.D
+
+	// filterGenderAndAge := bson.D{
+	// 	{"$and", bson.A{
+	// 		bson.D{
+	// 			{"gender", "F"},
+	// 			{"age", bson.D{{"$gte", 25}}},
+	// 		},
+	// 	}},
+	// }
+
+	// projection := bson.D{
+	// 	{"_id", 1}, 
+	// 	{"fullName", 1},
+	// 	{"gender", 1},
+	// 	{"age", 1},
+	// }
+
+	// filterGenderAndAgeResult := make([]*Student, 0)
+
+	// cursor, err := coll.Find(ctx, filterGenderAndAge, options.Find().SetProjection(projection))
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	// for cursor.Next(ctx) {
+	// 	var student Student
+	// 	err := cursor.Decode(&student)
+	// 	if err != nil {
+	// 		log.Println(err.Error())
+	// 	}
+	// 	filterGenderAndAgeResult = append(filterGenderAndAgeResult, &student)
+	// }
+
+	// for _, student := range filterGenderAndAge {
+	// 	fmt.Println("FILTER BY GENDER & AGE (WITH STRUCT)", student)
+	// }
+
+
+	//AGGREGATION
+
+	// coll = connect.Database("enigma").Collection("products")
+
+	// count, err := coll.CountDocuments(ctx, bson.D{})
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// fmt.Println("Product Total: ", count)
+
+	//with filter
+
+	// count, err = coll.CountDocuments(ctx, bson.D{{"category", "food"}})
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// fmt.Println("Product Total with category[food]: ", count)
+
+	//Match, group, sort, dll
+
+	matchStage := bson.D{
+		{
+			"$match", bson.D{
+				{"category", "Food"},
+			}},
+		}
+
+	groupStage := bson.D{
+		{"$group", bson.D{
+			{"_id", "$category"}, 
+			{"Total", bson.D{{"$sum", 1}}},
+			}},
+		}
+
+	cursor, err := coll.Aggregate(ctx, mongo.Pipeline{matchStage, groupStage})
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	var productCount []bson.M
+	err = cursor.All(ctx, &productCount)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	for _, product := range productCount {
+		fmt.Printf("Group[%v], Total[%v]\n ", product["_id"], product["count"])
+	}
+
+	
 
 
 }
